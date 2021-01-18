80170fd72420b9ad26e9e741b7f08d64bb6a9254
Gagandeep Kaur <20015660@saultcollege.ca>
2021-01-18 18:54:42 +0000

    Lab2  done

--- index.php
+++ index.php
@@ -1,12 +1,87 @@
-<?php
-	$name = "Gagandeep Kaur" ;
-	$date = "11/Jan/2021" ;
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
 
-<b>
-	<?php
-		echo $name ;
-	    echo "<br>" ;
-        echo $date ;
-	?>
-</b>
+table, th, td {
+  border: 1px solid black;
+}
+</style>
+</head>
+    <body>
+<?php
+$i=1;
+$j=1;
+$films = array(
+            "comedy" => array (
+                            0 => "Pink Panther",
+                           1 => "johnny English",
+                            2 => "Tommy Boy"),
+            "action" => array (
+                            0 => "Die Hard",
+                            1 => "Expendables"),
+            "epic" => array (
+                            0 => "The Lord of the Rings"),
+            "Romance" => array (
+                            0 => "Romeo and Juliet")
+);	
+
+$favorites = array(
+array (
+"Name" => "Dave Punk",
+"Phone" => "5689741523",
+"Email" => "davepunk@gmail.com"),
+array (
+"Name" => "Monty Smith",
+"Phone" => "2584369721",
+"Email" => "montysmith@gmail.com"),
+array (
+"Name" => "John Flinch",
+"Phone" => "9875147536",
+"Email" => "johnflinch@gmail.com")
+);
+echo '<p>First Table</p>';
+echo '<table>';
+foreach($films as $genre => $movies)
+{  
+
+        if($j==1)
+        {
+        echo '<th>MOVIES</th>';
+        echo '<th>GENRE</th>';
+        $j=0;
+        }
+	foreach($movies as $movie)
+	{   
+        echo '<tr>';
+		echo '<td>' .$movie. '</td>';
+        echo '<td>' .$genre. '</td>';
+        echo '</tr>';
+	}
+ 
+}
+echo '</table>';
+
+echo '<table>';
+echo '<p>Second Table</p>';
+
+   foreach($favorites as $details)
+   {
+   if($i==1)
+        {
+        echo '<th>NAME</th>';
+        echo '<th>Phone</th>';
+        echo '<th>Email</th>';
+        $i=0;
+        }
+    echo '<tr>';
+		echo '<td>' .$details["Name"]. '</td>';
+        echo '<td>' .$details["Phone"]. '</td>';
+        echo '<td>' .$details["Email"]. '</td>';
+        echo '</tr>';
+    }
+echo '</table>';
+		
+?>
+	</body>
+</html>
